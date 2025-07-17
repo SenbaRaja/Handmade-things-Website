c5a9788842cc600c501a503242d43fb989201105
SenbaRaja <senbaraja04@gmail.com>
2025-07-17 09:03:42 +0530

    Add files via upload

---
+++ hand.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("active");
+}
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
+function toggleMenu() {
+  document.querySelector(".nav-links").classList.toggle("active");
+}
+
+function addToCart(product, price) {
+  alert(`${product} added to cart for ₹${price}`);
+}
